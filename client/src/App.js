9324eb9d1f2c1d1098350201654cdfe31c0e5e0d
Grace Nieboer <grace.nieboer@outlook.com>
2023-07-05 23:24:35 -0400

    add navlinks for home and job pages

--- client/src/App.js
+++ client/src/App.js
@@ -3,11 +3,13 @@ import './App.css';
 import Home from './Home';
 import Jobs from './Jobs';
 import NoMatch from './NoMatch';
+import Nav from './Nav';
 import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
 
 function App() {
   return (
     <Router>
+      <Nav/>
       <Routes>
         <Route path='/' element={<Home/>}/>
         <Route path='/jobs' element={<Jobs/>}/>
--- client/src/Nav.js
+++ client/src/Nav.js
@@ -1,8 +1,11 @@
-
+import { NavLink } from "react-router-dom"
 
 export default function Nav(){
 
     return(
-        <></>
+        <nav>
+        <NavLink to="/" exact>HOME</NavLink>
+        <NavLink to="/jobs" exact>JOBS</NavLink>
+        </nav>
     )
 }
