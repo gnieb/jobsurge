db97d4c7da5a70bc8daafcd0e328f41bab812913
Grace Nieboer <grace.nieboer@outlook.com>
2023-07-08 19:28:03 -0400

    flexbox Home page

--- client/src/Home.js
+++ client/src/Home.js
@@ -4,12 +4,20 @@ export default function Home () {
     const [quote, setQuote] = useState("Take it one day at a time")
     
     return (
-        <>
-            <h1>welcome home</h1>
-            <h1>{quote}</h1>
-            <h1>remember to drink water </h1>
-            <h1>don't forget to move! </h1>
-
-        </>
+        <div>
+            <div className="flex flex-row justify-between m-5 p-5">
+                <div className = "m-5 p-5">
+            
+                    <p className="text-6xl">Did you drink water today?</p>
+                    
+                </div>
+            <div className="m-5 p-5">
+                <p className="text-3xl">{quote}</p>
+            </div>
+        </div>
+        <div className="flex items-center justify-center">
+            <p>don't forget to move around a little! Take a break, go for a walk.</p>
+        </div>
+        </div>
     )
 }
