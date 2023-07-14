65409ccd9a42791beb1a92b657a04ea60d599446
Grace Nieboer <grace.nieboer@outlook.com>
2023-07-14 09:46:05 -0400

    AddAdvice function built

---
+++ client/src/AddAdvice.js
@@ -0,0 +1,14 @@
+export default function AddAdvice () {
+    return (
+        <div>
+            <form>
+                <button>Add Advice</button>
+            </form>
+        
+        </div>
+    )
+     
+} 
+
+// install formik
+// add a way to add advice 
