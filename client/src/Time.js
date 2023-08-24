5a4e5f86a330f57493f3ec4c4865b3ebd21b1eb7
Grace Nieboer <grace.nieboer@outlook.com>
2023-08-23 21:42:24 -0400

    get dynamic day of the week

--- client/src/Time.js
+++ client/src/Time.js
@@ -8,12 +8,13 @@ export default function Time(){
             clearInterval(timer)
         }
     })
+    const days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
+    const weekDay = days[(date.getDay())]
     return(
         <div>
-            <div className="text-9xl">WEDNESDAY</div>
-            <div className="text-5xl">{date.toLocaleDateString()}</div>
-            <div className="text-5xl">{date.toLocaleTimeString()}</div>
-
+            <div className="text-3xl lg:text-[180px]">{weekDay.toUpperCase()}</div>
+            <div className="text-3xl sm:text-5xl">{date.toLocaleDateString()}</div>
+            <div className="text-3xl sm:text-5xl">{date.toLocaleTimeString()}</div>
         </div>
     )
 }
