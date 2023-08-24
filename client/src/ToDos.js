1b6a599ea52364caf0e2b00f42564d0360ab0128
Grace Nieboer <grace.nieboer@outlook.com>
2023-08-23 22:58:12 -0400

    create Todos component

---
+++ client/src/ToDos.js
@@ -0,0 +1,13 @@
+import { useEffect, useState } from "react"
+
+export default function ToDos(){
+    const [todos, setTodos] = useState([])
+    useEffect(() => {
+        
+    }, [])
+    return(
+        <div>
+            
+        </div>
+    )
+}
