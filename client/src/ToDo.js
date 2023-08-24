64ab2ceb97015e34e9acaf7e9108b97ea48ca031
Grace Nieboer <grace.nieboer@outlook.com>
2023-08-24 15:08:30 -0400

    basic ToDo component created

---
+++ client/src/ToDo.js
@@ -0,0 +1,7 @@
+export default function ToDo({t}){
+    return (
+        <div>
+
+        </div>
+    )
+}
--- client/src/ToDos.js
+++ client/src/ToDos.js
@@ -1,10 +1,25 @@
 import { useEffect, useState } from "react"
+import ToDo from "./ToDo"
 
 export default function ToDos(){
     const [todos, setTodos] = useState([])
     useEffect(() => {
-        
+        fetch('/todos')
+        .then(r => {
+            if(r.ok) {
+                r.json().then(data => {
+                    setTodos(data)
+                })
+            }
+        })
     }, [])
+
+    const displayToDos = todos.map(t => {
+        return (
+            <ToDo key={t.id} t={t} />
+        )
+    })
+
     return(
         <div>
             
